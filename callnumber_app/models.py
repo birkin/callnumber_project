3192678cbc084a9a6bb93e6eb4d1905c08d5fa10
birkin <birkin.diana@gmail.com>
2016-11-09 12:38:50 -0500

    adds model.

--- callnumber_app/models.py
+++ callnumber_app/models.py
@@ -8,3 +8,14 @@ from django.db import models
 from django.http import HttpResponseRedirect
 
 log = logging.getLogger(__name__)
+
+
+class Subject(models.Model):
+
+    name = models.CharField( max_length=100 )
+    slug = models.SlugField()
+    code_range = models.TextField()
+    note = models.TextField( blank=True )
+
+    def __unicode__(self):
+        return '%s' % self.name
