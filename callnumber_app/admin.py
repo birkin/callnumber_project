f0ad9c8bd79db41fb4bed266cc9aa24afc7d904e
birkin <birkin.diana@gmail.com>
2016-11-09 16:33:17 -0500

    adds admin.

--- callnumber_app/admin.py
+++ callnumber_app/admin.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+from .models import Subject
 from django.contrib import admin
+
+
+class SubjectAdmin( admin.ModelAdmin ):
+    list_display = ( 'name', 'code_range', 'note' )
+    search_fields = ( 'id', 'name', 'code_range', 'note' )
+
+
+admin.site.register(Subject, SubjectAdmin)
