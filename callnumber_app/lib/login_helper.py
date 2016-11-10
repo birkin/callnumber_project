09f1e56e628268af35eadaacfe1817c816cb376f
birkin <birkin.diana@gmail.com>
2016-11-10 15:46:15 -0500

    refactors login.

---
+++ callnumber_app/lib/login_helper.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+import logging
+from callnumber_app import settings_app
+from callnumber_app.lib.shib import ShibChecker
+from django.conf import settings
+from django.contrib.auth import authenticate
+
+
+log = logging.getLogger(__name__)
+shib_checker = ShibChecker()
+
+
+def get_user( meta_dct ):
+    if shib_checker.validate_user( meta_dct ):
+        log.debug( 'validated via shib' )
+        user = grab_good_user()
+    elif meta_dct['SERVER_NAME'] == '127.0.0.1' and settings.DEBUG == True:
+        log.debug( 'validated via localdev' )
+        user = grab_good_user()
+    else:
+        log.debug( 'not validated' )
+        user = None
+    return user
+
+
+def grab_good_user():
+    user = authenticate(username=settings_app.LEGIT_USER, password=settings_app.LEGIT_USER_PASSWORD)
+    return user
--- callnumber_app/views.py
+++ callnumber_app/views.py
@@ -1,21 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+
 import datetime, json, logging, os, pprint
-from callnumber_app import settings_app
-from callnumber_app.lib.shib import ShibChecker
-from callnumber_app.lib import login
-from django.conf import settings
-from django.contrib.auth import authenticate
+from callnumber_app.lib import login_helper
 from django.contrib.auth import login as django_login
-from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 
 
 log = logging.getLogger(__name__)
-shib_checker = ShibChecker()
 
 
 def hi( request ):
@@ -31,15 +26,7 @@ def assign_subject( request ):
 
 def login( request ):
     log.debug( 'starting login()' )
-    # user = lib.get_user( request.META )
-    if shib_checker.validate_user( request.META ):
-        log.debug( 'validated via shib' )
-        user = authenticate(username=settings_app.LEGIT_USER, password=settings_app.LEGIT_USER_PASSWORD)
-    elif request.META['SERVER_NAME'] == '127.0.0.1' and settings.DEBUG == True:
-        log.debug( 'validated via localdev' )
-        user = authenticate(username=settings_app.LEGIT_USER, password=settings_app.LEGIT_USER_PASSWORD)
-    else:
-        user = None
+    user = login_helper.get_user( request.META )
     if user:
         django_login(request, user )
     url = reverse('admin:callnumber_app_subject_changelist' )
