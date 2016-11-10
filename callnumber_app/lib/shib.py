5bfbeb7dd659f2a93265925b0f1fd3faf6cdad78
birkin <birkin.diana@gmail.com>
2016-11-10 15:31:54 -0500

    implements login.

---
+++ callnumber_app/lib/shib.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+
+class ShibChecker( object ):
+
+    def __init__( self ):
+        pass
+
+    def validate_user( self, meta_dct ):
+        return False
--- callnumber_app/views.py
+++ callnumber_app/views.py
@@ -4,7 +4,10 @@ from __future__ import unicode_literals
 import datetime, json, logging, os, pprint
 from callnumber_app import settings_app
 from callnumber_app.lib.shib import ShibChecker
-from django.contrib.auth import authenticate, login
+from callnumber_app.lib import login
+from django.conf import settings
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as django_login
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
@@ -28,13 +31,16 @@ def assign_subject( request ):
 
 def login( request ):
     log.debug( 'starting login()' )
+    # user = lib.get_user( request.META )
     if shib_checker.validate_user( request.META ):
+        log.debug( 'validated via shib' )
         user = authenticate(username=settings_app.LEGIT_USER, password=settings_app.LEGIT_USER_PASSWORD)
     elif request.META['SERVER_NAME'] == '127.0.0.1' and settings.DEBUG == True:
+        log.debug( 'validated via localdev' )
         user = authenticate(username=settings_app.LEGIT_USER, password=settings_app.LEGIT_USER_PASSWORD)
     else:
         user = None
     if user:
-        login(request, user )
+        django_login(request, user )
     url = reverse('admin:callnumber_app_subject_changelist' )
     return HttpResponseRedirect( url )
