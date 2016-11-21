5b81abb338d5af672dce94ad033ccc8bb1953d70
birkin <birkin.diana@gmail.com>
2016-11-21 13:48:49 -0500

    adds doc url to v1 dump call.

--- callnumber_app/views.py
+++ callnumber_app/views.py
@@ -3,14 +3,13 @@
 from __future__ import unicode_literals
 
 import datetime, json, logging, os, pprint
+from callnumber_app import settings_app
+from callnumber_app.lib import views_helper
 from callnumber_app.lib.login_helper import UserGrabber
 from django.contrib.auth import login as django_login
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-# from callnumber_app.models import Subject
-# from callnumber_app.lib import callnumber_normalizer, views_helper
-from callnumber_app.lib import views_helper
 
 
 log = logging.getLogger(__name__)
@@ -55,13 +54,30 @@ def data_v1( request ):
     if request.GET.get( 'data', '' ) == 'dump':
         return_values = dump_param_handler.grab_all_v1()
         service_response = {'data': 'dump'}
-
     elif 'callnumber' in request.GET:
         call_param_handler = views_helper.CallParamHandler()
         return_values = call_param_handler.grab_callnumbers( request.GET['callnumber'].split() )
     service_response['result'] = {}
     service_response['result']['items'] = return_values
-    service_response['result']['service_documentation'] = 'coming soon'
-    service_response['result']['service_contact'] = 'coming soon'
+    service_response['result']['service_documentation'] = settings_app.DOCS_URL
     output = json.dumps( service_response, sort_keys=True, indent=2 )
     return HttpResponse( output, content_type='application/json')
+
+
+# def data_v1( request ):
+#     """ Handles all /v1/ urls. """
+#     dump_param_handler = views_helper.DumpParamHandler()
+#     service_response = {}
+#     if request.GET.get( 'data', '' ) == 'dump':
+#         return_values = dump_param_handler.grab_all_v1()
+#         service_response = {'data': 'dump'}
+
+#     elif 'callnumber' in request.GET:
+#         call_param_handler = views_helper.CallParamHandler()
+#         return_values = call_param_handler.grab_callnumbers( request.GET['callnumber'].split() )
+#     service_response['result'] = {}
+#     service_response['result']['items'] = return_values
+#     service_response['result']['service_documentation'] = 'coming soon'
+#     service_response['result']['service_contact'] = 'coming soon'
+#     output = json.dumps( service_response, sort_keys=True, indent=2 )
+#     return HttpResponse( output, content_type='application/json')
