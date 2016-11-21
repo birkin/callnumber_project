c3bbe6db8930ca16f9b0da676c551c974710b025
birkin <birkin.diana@gmail.com>
2016-11-21 10:04:42 -0500

    adds new url for differently-formatted api response.

--- callnumber_app/urls_app.py
+++ callnumber_app/urls_app.py
@@ -10,7 +10,9 @@ urlpatterns = [
 
     url( r'^info/$', views.hi, name='info_url' ),
 
-    url( r'^v1/', views.data, name='data_url' ),
+    url( r'^v1/', views.data_v1, name='data_v1_url' ),
+
+    url( r'^v2/', views.data_v2, name='data_v2_url' ),
 
     url( r'^login/', views.login, name='login_url' ),
 
