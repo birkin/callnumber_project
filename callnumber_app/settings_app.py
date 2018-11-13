aaf9975d01e8b37910019dd8e2f8c90da022722a
birkin <birkin.diana@gmail.com>
2018-11-13 13:18:57 -0500

    py3 improvements.

--- callnumber_app/settings_app.py
+++ callnumber_app/settings_app.py
@@ -4,10 +4,10 @@ from __future__ import unicode_literals
 import json, os
 
 
-DOCS_URL = unicode( os.environ['CLLNMBR__DOCS_URL'] )
+DOCS_URL = os.environ['CLLNMBR__DOCS_URL']
 
-LEGIT_USER = unicode( os.environ['CLLNMBR__LEGIT_USER'] )
-LEGIT_USER_PASSWORD = unicode( os.environ['CLLNMBR__LEGIT_USER_PASSWORD'] )
+LEGIT_USER = os.environ['CLLNMBR__LEGIT_USER']
+LEGIT_USER_PASSWORD = os.environ['CLLNMBR__LEGIT_USER_PASSWORD']
 
 LEGIT_GROUPER_GROUPS = json.loads( os.environ['CLLNMBR__LEGIT_GROUPS_JSON'] )
 LEGIT_EPPNS = json.loads( os.environ['CLLNMBR__LEGIT_EPPNS_JSON'] )
